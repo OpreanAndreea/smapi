2293f21fcefba0d764451b331ff3f5f95361cf76
OpreanAndreea <andreea.oprean@ibm.com>
2026-07-07 17:30:07 +0300

    SMAPI Project Setup

---
+++ src/main/java/com/coding_project/smapi/SmapiApplication.java
@@ -0,0 +1,15 @@
+package com.coding_project.smapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmapiApplication.class, args);
+
+		System.out.println("SmapiApplication");
+	}
+
+}
---
+++ src/test/java/com/coding_project/smapi/SmapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding_project.smapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
